71a89d4789e8c4891900416227176618d0efad5d
LucasInmanuel <lucasemanuel2077@gmail.com>
2023-03-14 14:27:55 -0300

    fixing images and responsiveness

--- js/content.js
+++ js/content.js
@@ -1,18 +1,40 @@
 const filmRolls = [
     {
-        "part1": {
-            "pathImage": "./images/rua-do-ouvidor.jpg",
-            "title": "Rua do Ouvidor",
-            "overview": "A Rua do Ouvidor é uma das ruas mais antigas do Rio de Janeiro, já existindo por volta do ano de 1578, segunda metade do século 16, quando entrão era chamada de Rua Desvio do Mar."
-        },
-        "part2": {},
-        "part3": {}
+        "id": 1,
+        "pathImage": "../images/rua-do-ouvidor.jpg",
+        "title": "Rua do Ouvidor",
+        "overview": "A Rua do Ouvidor é uma das ruas mais antigas do Rio de Janeiro, já existindo por volta do ano de 1578, segunda metade do século 16, quando entrão era chamada de Rua Desvio do Mar."
     },
     {
-        "part1": {},
-        "part2": {},
-        "part3": {}
-    }
+        "id": 2,
+        "pathImage": "../images/rua-do-ouvidor.jpg",
+        "title": "Rua do Ouvidor",
+        "overview": "A Rua do Ouvidor é uma das ruas mais antigas do Rio de Janeiro, já existindo por volta do ano de 1578, segunda metade do século 16, quando entrão era chamada de Rua Desvio do Mar."
+    },
+    {
+        "id": 3,
+        "pathImage": "../images/rua-do-ouvidor.jpg",
+        "title": "Rua do Ouvidor",
+        "overview": "A Rua do Ouvidor é uma das ruas mais antigas do Rio de Janeiro, já existindo por volta do ano de 1578, segunda metade do século 16, quando entrão era chamada de Rua Desvio do Mar."
+    },
+    {
+        "id": 4,
+        "pathImage": "../images/rua-do-ouvidor.jpg",
+        "title": "Rua do Ouvidor",
+        "overview": "A Rua do Ouvidor é uma das ruas mais antigas do Rio de Janeiro, já existindo por volta do ano de 1578, segunda metade do século 16, quando entrão era chamada de Rua Desvio do Mar."
+    },
+    {
+        "id": 5,
+        "pathImage": "../images/rua-do-ouvidor.jpg",
+        "title": "Rua do Ouvidor",
+        "overview": "A Rua do Ouvidor é uma das ruas mais antigas do Rio de Janeiro, já existindo por volta do ano de 1578, segunda metade do século 16, quando entrão era chamada de Rua Desvio do Mar."
+    },
+    {
+        "id": 6,
+        "pathImage": "../images/rua-do-ouvidor.jpg",
+        "title": "Rua do Ouvidor",
+        "overview": "A Rua do Ouvidor é uma das ruas mais antigas do Rio de Janeiro, já existindo por volta do ano de 1578, segunda metade do século 16, quando entrão era chamada de Rua Desvio do Mar."
+    },
 ]
 
 export default filmRolls;
--- js/index.js
+++ js/index.js
@@ -1,37 +1,220 @@
 import content from "./content.js";
-let filmRollWrapper = document.getElementById("film-roll-wrapper")
 
-let filmRolls = document.getElementsByClassName("film-roll");
-console.log(content)
-for(let i = 0;i < filmRolls.length;i++){
-    let parts = document.getElementsByClassName("part")
+let movieBtn = document.getElementById("movie-btn");
+let bgMovie = document.getElementById("bg-movie");
 
-    content
-}
+movieBtn.addEventListener("click",()=>{
+    bgMovie.innerHTML = `
+        <video muted autoplay loop>
+            <source src="./videos/old-cinema-filter.mp4" type="video/mp4">
+        </video>
+    `
+    configFilmRolls()
+})
 
-content.forEach((v,i)=>{
-    filmRolls.item(i)
-    filmRollWrapper.innerHTML += `
-    <div class="film-roll">
-        <div class="outline-1">
-            <div class="outline-2">
-                <div class="part">
-                    <div class="item"></div>
+window.addEventListener("resize",()=>{
+    console.log(content[0].pathImage)
+    configFilmRolls()
+})
+
+function configFilmRolls(){
+let filmRollWrapper = document.getElementById("film-roll-wrapper");
+
+    let viewWidth = window.innerWidth
+    if(viewWidth> 1024){
+            filmRollWrapper.innerHTML = `
+                <div style="top:16vh;transform: skew(5deg, 2deg);" class="film-roll">
+                    <div class="outline-1">
+                        <div class="outline-2">
+                            <div class="part">
+                                <div class="placeholder-item"></div>
+                            </div>
+                            <div class="part">
+                                <div style="background-image: url(${content[0].pathImage})" class="item"></div>
+                            </div>
+                            <div class="part">
+                                <div style="background-image: url(${content[1].pathImage})" class="item"></div>
+                            </div>
+                            <div class="part">
+                                <div style="background-image: url(${content[2].pathImage})" class="item"></div>
+                            </div>
+                            <div class="part">
+                                <div class="placeholder-item"></div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div class="part">
-                    <img class="item" src="../images/rua-do-ouvidor.jpg" />
+                <div style="top:150vh;transform: skew(-5deg, -2deg);" class="film-roll">
+                    <div class="outline-1">
+                        <div class="outline-2">
+                            <div class="part">
+                                <div class="placeholder-item"></div>
+                            </div>
+                            <div class="part">
+                                <div style="background-image: url(${content[3].pathImage})" class="item"></div>
+                            </div>
+                            <div class="part">
+                                <div style="background-image: url(${content[4].pathImage})" class="item"></div>
+                            </div>
+                            <div class="part">
+                                <div style="background-image: url(${content[5].pathImage})" class="item"></div>
+                            </div>
+                            <div class="part">
+                                <div class="placeholder-item"></div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div class="part">
-                    <img class="item" src="../images/rua-do-ouvidor.jpg" />
-                </div>
-                <div class="part">
-                    <img class="item" src="../images/rua-do-ouvidor.jpg" />
-                </div>
-                <div class="part">
-                    <div class="item"></div>
+            `
+    }else if(viewWidth > 720){
+        filmRollWrapper.innerHTML = `
+            <div style="top:16vh;transform: skew(5deg, 2deg);" class="film-roll">
+                <div class="outline-1">
+                    <div class="outline-2">
+                        <div class="part">
+                            <div class="placeholder-item"></div>
+                        </div>
+                        <div class="part">
+                            <div style="background-image: url(${content[0].pathImage})" class="item"></div>
+                        </div>
+                        <div class="part">
+                            <div style="background-image: url(${content[1].pathImage})" class="item"></div>
+                        </div>
+                        <div class="part">
+                            <div class="placeholder-item"></div>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
-    </div>
-`
-})
+            <div style="top:150vh;transform: skew(-5deg, -2deg);" class="film-roll">
+                <div class="outline-1">
+                    <div class="outline-2">
+                        <div class="part">
+                            <div class="placeholder-item"></div>
+                        </div>
+                        <div class="part">
+                            <div style="background-image: url(${content[2].pathImage})" class="item"></div>
+                        </div>
+                        <div class="part">
+                            <div style="background-image: url(${content[3].pathImage})" class="item"></div>
+                        </div>
+                        <div class="part">
+                            <div class="placeholder-item"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div style="top:284vh;transform:skew(5deg, 2deg);" class="film-roll">
+                <div class="outline-1">
+                    <div class="outline-2">
+                        <div class="part">
+                            <div class="placeholder-item"></div>
+                        </div>
+                        <div class="part">
+                            <div style="background-image: url(${content[4].pathImage})" class="item"></div>
+                        </div>
+                        <div class="part">
+                            <div style="background-image: url(${content[5].pathImage})" class="item"></div>
+                        </div>
+                        <div class="part">
+                            <div class="placeholder-item"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+         `
+    }else{
+        filmRollWrapper.innerHTML = `
+            <div style="top:16vh;transform: skew(5deg, 2deg);" class="film-roll">
+                <div class="outline-1">
+                    <div class="outline-2">
+                        <div class="part">
+                            <div class="placeholder-item"></div>
+                        </div>
+                        <div class="part">
+                            <div style="background-image: url(${content[0].pathImage})" class="item"></div>
+                        </div>
+                        <div class="part">
+                            <div class="placeholder-item"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div style="top:150vh;transform: skew(-5deg, -2deg);" class="film-roll">
+                <div class="outline-1">
+                    <div class="outline-2">
+                        <div class="part">
+                            <div class="placeholder-item"></div>
+                        </div>
+                        <div class="part">
+                            <div style="background-image: url(${content[1].pathImage})" class="item"></div>
+                        </div>
+                        <div class="part">
+                            <div class="placeholder-item"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div style="top:284vh;transform:skew(5deg, 2deg);" class="film-roll">
+                <div class="outline-1">
+                    <div class="outline-2">
+                        <div class="part">
+                            <div class="placeholder-item"></div>
+                        </div>
+                        <div class="part">
+                            <div style="background-image: url(${content[2].pathImage})" class="item"></div>
+                        </div>
+                        <div class="part">
+                            <div class="placeholder-item"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div style="top:418vh;transform:skew(-5deg, -2deg);" class="film-roll">
+                <div class="outline-1">
+                    <div class="outline-2">
+                        <div class="part">
+                            <div class="placeholder-item"></div>
+                        </div>
+                        <div class="part">
+                            <div style="background-image: url(${content[3].pathImage})" class="item"></div>
+                        </div>
+                        <div class="part">
+                            <div class="placeholder-item"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div style="top:552vh;transform:skew(5deg, 2deg);" class="film-roll">
+                <div class="outline-1">
+                    <div class="outline-2">
+                        <div class="part">
+                            <div class="placeholder-item"></div>
+                        </div>
+                        <div class="part">
+                            <div style="background-image: url(${content[4].pathImage})" class="item"></div>
+                        </div>
+                        <div class="part">
+                            <div class="placeholder-item"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div style="top:686vh;transform:skew(-5deg, -2deg);" class="film-roll">
+                <div class="outline-1">
+                    <div class="outline-2">
+                        <div class="part">
+                            <div class="placeholder-item"></div>
+                        </div>
+                        <div class="part">
+                            <div style="background-image: url(${content[5].pathImage})" class="item"></div>
+                        </div>
+                        <div class="part">
+                            <div class="placeholder-item"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+         `
+    }
+}
